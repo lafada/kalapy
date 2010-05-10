a6c4a1273668b34e0197f131cdc11724f8f61a3e
Amit Mendapara <mendapara.amit@gmail.com>
2010-05-11 01:19:17 +0530

    Implemented simple signal dispatcher

---
+++ rapido/core/signal.py
@@ -0,0 +1,154 @@
+"""This module implements simple signal dispatcher.
+
+Connecting a signal is as easy as using a `signal.connect` decorator
+with signal name on a handler function. The signal can be fired with 
+`signal.send` method along with params if any.
+
+For example:
+
+>>> @signal.connect('onfinish')
+>>> def on_finish_1(state):
+>>>     pass
+
+>>> @signal.connect('onfinish')
+>>> def on_finish_2(state):
+>>>     pass
+
+The signal can be fired like this:
+
+>>> signal.send('onfinish', state=1)
+
+In this case both the handlers connected to the 'onfinish' signal will 
+be fired.
+"""
+import types, inspect, weakref
+
+
+__all__ = ('Signal', 'connect', 'disconnect', 'send')
+
+
+class Signal(object):
+    """Signal class caches all the registered handlers in WeakValueDictionary
+    so that handlers can be automatically garbage collected if the reference
+    to the handler is the only reference.
+    """
+    def __init__(self, name):
+        """Create a new Signal instance with the given name.
+        """
+        self.name = name
+        self.registry = weakref.WeakValueDictionary()
+        
+    def make_id(self, handler):
+        if hasattr(handler, 'im_func'):
+            return id(handler.im_self), id(handler.im_func)
+        return id(handler)
+
+    def connect(self, handler):
+        """Connect the given handler to the signal. The handler must be a 
+        function.
+
+        Args:
+            handler: a signal handler
+
+        Return:
+            returns the handler itself
+
+        Raises:
+            TypeError: if handler is not a function
+        """
+        if not isinstance(handler, types.FunctionType):
+            raise TypeError('Signal handler must be a function')
+        id = self.make_id(handler)
+        if id not in self.registry:
+            self.registry[id] = handler
+        return handler
+
+    def disconnect(self, handler):
+        """Manually disconnect the given handler if it is registered with
+        this signal.
+
+        By default, the handler will be removed automatically from the signal
+        registry if it is the only reference remained.
+
+        Args:
+            handler: the handler function
+        """
+        id = self.make_id(handler)
+        self.registry.pop(id, None)
+
+    def send(self, *args, **kw):
+        """Fire the signal.
+
+        All handlers registered with this signal instance will be called passing
+        the given *args nad **kw
+
+        Args:
+            *args: to be passed to signal handlers
+            **kw: to be passed to signal handlers
+
+        Returns:
+            list of results returned by all the handlers
+        """
+        result = []
+        for handler in self.registry.values():
+            result.append(handler(*args, **kw))
+        return result
+
+
+registry = {}
+
+def connect(signal):
+    """A decorator to connect a function to the specified signal.
+
+    >>> @signal.connect('onfinish')
+    >>> def on_finish_1(state):
+    >>>     pass
+
+    Args:
+        signal: name of the signal
+    """
+    def wrapper(func):
+        registry.setdefault(signal, Signal(signal)).connect(func)
+        return func
+    return wrapper
+
+def disconnect(signal, handler=None):
+    """If handler is given then disconnect the handler from the
+    specified signal else disconnect all the handlers of the given
+    signal.
+
+    >>> signal.disconnect('onfinish', on_finish_1)
+    >>> signal.dispatcher('onfinish')
+
+    Args:
+        signal: name of the signal
+        handler: a signal handler
+    """
+    if signal not in registry:
+        return
+    if handler:
+        registry[signal].disconnect(handler)
+        return
+    registry[signal].registry.clear()
+    del registry[signal]
+
+
+def send(signal, *args, **kw):
+    """Fire the specified signal.
+
+    The signal handlers will be called with the given *args and **kw.
+
+    >>> signal.send('onfinish', state=1)
+
+    Args:
+        *args: to be passed to the signal handlers
+        **kw: to be passed to the signal handlers
+
+    Returns:
+        list of the results of all the signal handlers
+    """
+    result = []
+    if signal in registry:
+        result.extend(registry[signal].send(*args, **kw))
+    return result
+
