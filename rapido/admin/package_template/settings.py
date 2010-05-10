221ca3063b9665aef22a8fffef11af23d977855b
Amit Mendapara <mendapara.amit@gmail.com>
2010-05-10 16:54:52 +0530

    Updated project/package skeleton templates

---
+++ rapido/admin/package_template/settings.py
@@ -0,0 +1,17 @@
+# Settings for ${name} package
+
+# Name of the package.
+NAME = "${name}"
+
+# Descriptive name of the package.
+TITLE = "${name} Application"
+
+# Version of the package.
+VERSION = "1.0"
+
+# Application description.
+DESCRIPTION = ""
+
+# List of other packages on which this package depends.
+DEPENDS = []
+
--- rapido/admin/project_template/settings.py
+++ rapido/admin/project_template/settings.py
@@ -1,17 +1,36 @@
-# settings for ${name} project
+# Settings for ${name} project
 
-DATABASE_ENGINE = ""        # sqlite3, postgresql, mysql, bigtable, simpledb, zodb
-DATABASE_NAME = ""          # database name, path if sqlite3 else depends on engine
-DATABASE_USER = ""          # database admin user, depends on engine
-DATABASE_PASSWORD = ""      # database admin password, depends on engine
-DATABASE_HOST = ""          # empty string for default value, depends on engine
-DATABASE_PORT = ""          # empty string for default value, depends on engine
-
-USE_I18N = True             # enable/disable internationalization support
-
+# Name of the project
 PROJECT_NAME = "${name}"
+
+# Version of the project
 PROJECT_VERSION = "1.0"
 
-INSTALLED_PACKAGES = (
-)
+# Database backend engine
+# Possible value can be either sqlite3, postgresql, bigtable
+DATABASE_ENGINE = ""
+
+# Database name
+# For sqlite3 use path to the sqlite3 database file
+# For bigtable keep empty
+DATABASE_NAME = ""
+
+# Database user (must have rights to create database tables)
+# Keep empty for sqlite3 and bigtable
+DATABASE_USER = ""
+
+# Database password
+# Keep empty for sqlite3 and bigtable
+DATABASE_PASSWORD = ""
+
+# Database host
+# Keep empty for sqlite3 and bigtable
+DATABASE_HOST = ""
+
+# Database port
+# Keep empty for sqlite3 and bigtable
+DATABASE_PORT = ""
+
+# Enable/Disable internationalization support
+USE_I18N = True
 
