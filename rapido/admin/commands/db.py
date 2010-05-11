47bb607083ce15b319c984c616ef1fef15c8584b
Amit Mendapara <mendapara.amit@gmail.com>
2010-05-11 22:13:36 +0530

    Improved database admin commands

--- rapido/admin/commands/db.py
+++ rapido/admin/commands/db.py
@@ -1,5 +1,11 @@
 from optparse import make_option
+
+from rapido.conf import settings
 from rapido.admin import BaseCommand
+
+from rapido import db
+from rapido.db import transaction
+from rapido.db.engines import database
 
 
 class DBCommand(BaseCommand):
@@ -10,9 +16,9 @@ class DBCommand(BaseCommand):
     scope = 'package'
 
     options = (
-        make_option('-I', '--info', help='Show the table schema for given packages', 
+        make_option('-I', '--info', help='Show the table schema for the given packages', 
             action='store_true'),
-        make_option('-S', '--sync', help="Create the database tables for all the INSTALLED_PACKAGES whose tables haven't been created.", 
+        make_option('-S', '--sync', help="Create the database tables for all the INSTALLED_PACKAGES whose tables haven't been created yet.", 
             action='store_true'),
         make_option('-B', '--backup', help='Create backup of the database.',
             metavar='FILE'),
@@ -23,103 +29,98 @@ class DBCommand(BaseCommand):
     exclusive = ('-I', '-S', '-B', '--reset')
 
     def execute(self, *args, **options):
-        if options.get('info'):
-            return self.info(*args)
-        if options.get('sync'):
-            return self.sync()
-        if options.get('reset'):
-            return self.reset()
-        if options.get('backup'):
-            return self.backup(options['backup'])
+        
+        if settings.DATABASE_ENGINE == "dummy":
+            raise self.error("DATABASE_ENGINE is not configured.")
+        
+        database.connect()
+        try:
+            if options.get('info'):
+                return self.info(*args)
+            if options.get('sync'):
+                return self.sync()
+            if options.get('reset'):
+                return self.reset()
+            if options.get('backup'):
+                return self.backup(options['backup'])
+        finally:
+            database.close()
         self.print_help()
 
     def get_models(self, *packages):
+        """Similar to db.get_models but returns a tuple, (list of models, 
+        list of models referenced from other packages)
         
-        from rapido import db
+        Where list of models will be sorted according to relationships to ensure
+        a model comes next to models it referencing.
         
+        Args:
+            *packages: package names
+            
+        Returns:
+            tuple
+        """
         models = db.get_models(*packages)
         
         result = []
         pending = []
         
-        def load_models(args):
+        def adjust_models(args):
             for model in args:
                 if model in result or model in pending:
                     continue
                 if model._meta.ref_models:
-                    load_models([m for m in model._meta.ref_models if m != model])
+                    adjust_models([m for m in model._meta.ref_models if m != model])
                 if model in models:
                     result.append(model)
                 else:
                     pending.append(model)
 
-        load_models(models)
+        adjust_models(models)
 
         return result, pending
 
     def info(self, *packages):
-        if not packages:
-            self.error("At least one package name required.")
-
-        from rapido.conf import settings
-        from rapido.db.engines import database
-
-        if settings.DATABASE_ENGINE == "dummy":
-            raise self.error("DATABASE_ENGINE is not configured.")
-
-        for package in packages:
-            if package not in settings.INSTALLED_PACKAGES:
-                self.error('%r not in INSTALLED_PACKAGES' % package)
-                
-        database.connect()
-
         models, pending = self.get_models(*packages)
-        try:
-            for model in models:
-                print database.schema_table(model)
-
-            if pending:
-                print '\n-- the following tables should also be added (from other packages)\n'
-                for model in pending:
-                    print '  --', model._meta.table
-                print
-        finally:
-            database.close()
+        for model in models:
+            print database.schema_table(model)
+        if pending:
+            print '\n-- the following tables should also be added (from other packages)\n'
+            for model in pending:
+                print '  --', model._meta.table
+            print
 
     def sync(self):
-        from rapido.conf import settings
-        from rapido.db.engines import database
-        
-        if settings.DATABASE_ENGINE == "dummy":
-            raise self.error("DATABASE_ENGINE is not configured.")
-
-        database.connect()
-                
         models, __pending = self.get_models()
         try:
             for model in models:
                 database.create_table(model)
+        except:
+            database.rollback()
+            raise
+        else:
             database.commit()
-        finally:
-            database.close()
             
     def reset(self):
-        from rapido.conf import settings
-        from rapido.db.engines import database
         
-        if settings.DATABASE_ENGINE == "dummy":
-            raise self.error("DATABASE_ENGINE is not configured.")
+        ans = raw_input("""
+Be careful, all the tables of database %r will be dropped.
+All the data stored in the database will be lost too.
 
-        database.connect()
+Are you sure about this action? (y/N): """ % settings.DATABASE_NAME)
+
+        if ans.lower() != 'y': return
         
         models, __pending = self.get_models()
         models.reverse()
         try:
             for model in models:
                 database.drop_table(model._meta.table)
+        except:
+            database.rollback()
+            raise
+        else:
             database.commit()
-        finally:
-            database.close()
 
     def backup(self, dest):
         pass
