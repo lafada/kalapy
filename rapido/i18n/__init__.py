5e37f8036174eff75e79b4b2ca9c3c9dff8f65a4
Amit Mendapara <mendapara.amit@gmail.com>
2010-06-10 17:12:21 +0530

    The gettext & ngettext are imported in i18n package

--- rapido/i18n/__init__.py
+++ rapido/i18n/__init__.py
@@ -1,5 +1,5 @@
-from rapido.i18n import utils
+from rapido.i18n.utils import gettext, ngettext
 
 # Install `gettext` aliased as `_` as a builtin
-__builtins__['_'] = utils.gettext
+__builtins__['_'] = gettext
 
